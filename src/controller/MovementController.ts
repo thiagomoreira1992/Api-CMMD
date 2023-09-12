3f01900ddaedc50e04c6e7e3348d8b1feabc06f7
Thiago Moreira <thiagomoreira.ifsc@gmail.com>
2023-09-11 21:55:25 -0300

    ....

--- src/controller/MovementController.ts
+++ src/controller/MovementController.ts
@@ -2,53 +2,73 @@ import { Request, Response } from "express";
 import { prisma } from "../database/client";
 import AppError from "../utils/AppError";
 
+
+
 export class MovementController {
-    async listAll(req: Request, res: Response) {
+    async listAll(res: Response) {
         const movements = await prisma.movement.findMany();
 
         return res.status(202).json({ movements })
     }
 
     async create(req: Request, res: Response) {
-        const { recordId, quantity } = req.body;
-        const  userId = req.user?.id;
-
+        const { batch, quantity } = req.body;
+        const userId = req.user?.id;
 
         try {
+
+            // Validar a entrada 'batch'
+            if (typeof batch !== 'string' || batch.trim() === '') {
+                throw new AppError('Batch deve ser uma string não vazia', 400);
+            }
+
+            // Validar a entrada 'quantity'
+            if (typeof quantity !== 'number' || isNaN(quantity) || quantity <= 0) {
+                throw new AppError('Quantity deve ser um número positivo', 400);
+            }
+
+            if(typeof userId !== 'number'|| isNaN(userId)|| userId <= 0){
+                throw new AppError('User has been logged', 400)
+            }
+
             const record = await prisma.record.findUnique({
-                where:{
-                    id: recordId
+                where: {
+                    batch
                 }
             })
 
-            if(!record){
+            if (!record) {
                 throw new AppError("This Record not exists", 401)
             }
-            
-            if((record.quantity + quantity) < 0){
+
+            if ((record.quantity + quantity) < 0) {
                 throw new AppError("This record not have this quantity", 401)
             }
 
             const newRecord = await prisma.record.update({
-                where:{
+                where: {
                     id: record.id
-                }, data:{
-                    quantity : record.quantity + quantity
+                }, data: {
+                    quantity: record.quantity + quantity
                 }
             })
 
             const movement = await prisma.movement.create({
-                data:{
-                    recordId,
+                data: {
+                    recordId: record.id,
                     quantity,
                     userId
                 }
             })
 
-            return res.status(201).json({newRecord, movement})
+            return res.status(201).json({ newRecord, movement })
         }
-        catch (Error: any) {
-            return res.status(Error.statusCode).json({ Error })
+        catch (error) {
+            if(error instanceof AppError){
+                return res.status(error.statusCode).json({ error : error.message})
+            }else{
+                return res.status(500).json({ error : "Erro interno"})
+            }
         }
     }
 }
--- src/utils/AppError.ts
+++ src/utils/AppError.ts
@@ -1,9 +1,10 @@
 
-export default class AppError{
+export default class AppError extends Error{
     message: string;
     statusCode: number;
 
     constructor(message: string, statusCode = 400){
+        super(message)
         this.message = message;
         this.statusCode = statusCode
     }
